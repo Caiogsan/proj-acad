bf60b31ebe72522c63645f51d979ece6eb2eb3d6
Caio Gonçalves <caiogsan@hotmail.com>
2023-10-05 19:43:10 -0300

    last

--- src/components/items/ContactSlide.js
+++ src/components/items/ContactSlide.js
@@ -2,15 +2,15 @@ import {FaFacebook, FaInstagram, FaWhatsapp} from 'react-icons/fa'
 
 function ContactSlide(){
     return (
-        <div className="flex w-[90%] h-[740px] xl:h-[940px] 2xl:h-[1140px] pb-8 m-auto">
+        <div className="flex w-[90%] h-[740px] xl:h-[940px] 2xl:h-[1000px] pb-8 m-auto">
             <div className="bg-gray-300 rounded border-2 border-black border-r-0 w-[50%]">   
                 <form className='m-auto flex flex-col justify-between h-full py-12 w-[80%]'>
                     <h1 className='text-center font-bold text-2xl'>Envie uma mensagem!</h1>
-                    <input className='text-sm rounded p-2' type="text" placeholder="Nome"></input>
-                    <input className='text-sm rounded p-2' type="email" placeholder="Email"></input>
-                    <input className='text-sm rounded p-2' type="text" placeholder="Assunto"></input>
-                    <textarea placeholder="Mensagem" className="resize-none h-24 rounded p-2"></textarea>
-                    <button type="submit" className='text-sm rounded hover:bg-yellow-400 duration-500 p-2 bg-white text-black'>Enviar!</button>
+                    <input className='text-sm 2xl:text-lg rounded p-2' type="text" placeholder="Nome"></input>
+                    <input className='text-sm 2xl:text-lg rounded p-2' type="email" placeholder="Email"></input>
+                    <input className='text-sm 2xl:text-lg rounded p-2' type="text" placeholder="Assunto"></input>
+                    <textarea placeholder="Mensagem" className="resize-none h-24 2xl:h-40 rounded p-2"></textarea>
+                    <button type="submit" className='text-sm 2xl:text-lg rounded hover:bg-yellow-400 duration-500 p-2 bg-white text-black'>Enviar!</button>
                     <div className='flex justify-center items-center gap-12'>
                         <a href="/" className='text-3xl hover:text-blue-800 duration-500'><FaFacebook></FaFacebook></a>
                         <a href="/" className='text-3xl hover:text-rose-600 duration-500'><FaInstagram></FaInstagram></a>
